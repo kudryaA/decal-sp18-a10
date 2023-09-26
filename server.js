0e7b2c06546632bbd9fd78e0a963253f43105f3f
T <kam123ua@gmail.com>
2023-09-26 18:12:22 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ var app = express(); 						// create our app w/ express
 var mongoose = require('mongoose'); 				// mongoose for mongodb
 var port = process.env.PORT || 8080; 				// set the port
 var database = require('./config/database'); 			// load the database config
-var morgan = require('morgan');
+//var morgan = require('morgan');
 var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
+//var methodOverride = require('method-override');
 
 // configuration ===============================================================
 mongoose.connect(database.mongoUrl); 	// Connect to local MongoDB instance. A remoteUrl is also available (modulus.io)
 
 app.use(express.static('./public')); 		// set the static files location /public/img will be /img for users
-app.use(morgan('dev')); // log every request to the console
+//app.use(morgan('dev')); // log every request to the console
 app.use(bodyParser.urlencoded({'extended': 'true'})); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({type: 'application/vnd.api+json'})); // parse application/vnd.api+json as json
